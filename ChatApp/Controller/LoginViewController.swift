feb2ead976a6da92191fc6a8ae9e5d19e540f67c
ayukin <uh1ohdpwgm@gmail.com>
2020-09-29 22:58:52 +0900

    commit

--- ChatApp/Controller/LoginViewController.swift
+++ ChatApp/Controller/LoginViewController.swift
@@ -19,6 +19,9 @@ class LoginViewController: UIViewController {
         
         IQKeyboardManager.shared.enable = true
         
+        loginMailTextField.delegate = self
+        loginPassTextField.delegate = self
+        
         loginButton.layer.masksToBounds = true
         loginButton.layer.cornerRadius = 3
     }
@@ -33,13 +36,26 @@ class LoginViewController: UIViewController {
         let storyboard: UIStoryboard = UIStoryboard(name: "SignUp", bundle: nil)
         let signUpVC = storyboard.instantiateViewController(withIdentifier: "SignUpVC") as! SignUpViewController
         self.present(signUpVC, animated: true, completion: nil)
-//        self.navigationController?.pushViewController(signUpVC, animated: true)
     }
-    
     
 }
 
 extension LoginViewController: UITextFieldDelegate {
+    // textFieldでテキスト選択が変更された時に呼ばれるメソッド
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        // textFieldが空かどうかの判別するための変数(Bool型)で定義
+        let mailIsEmpty = loginMailTextField.text?.isEmpty ?? true
+        let passIsEmpty = loginPassTextField.text?.isEmpty ?? true
+        // 全てのtextFieldが記入済みの場合の処理
+        if mailIsEmpty || passIsEmpty {
+            loginButton.isEnabled = false
+            loginButton.backgroundColor = UIColor.systemGray2
+        } else {
+            loginButton.isEnabled = true
+            loginButton.backgroundColor = UIColor(red: 4/255, green: 185/255, blue: 2/255, alpha: 1)
+        }
+    }
+    
     // textField以外の部分を押したときキーボードが閉じる
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
--- ChatApp/Controller/SignUpViewController.swift
+++ ChatApp/Controller/SignUpViewController.swift
@@ -10,8 +10,7 @@ import IQKeyboardManagerSwift
 
 class SignUpViewController: UIViewController {
     
-    @IBOutlet weak var userImage: UIButton!
-    
+    @IBOutlet weak var profileImageButton: UIButton!
     @IBOutlet weak var signUpMailTextField: UITextField!
     @IBOutlet weak var signUpPassTextField: UITextField!
     @IBOutlet weak var signUpUserNameTextField: UITextField!
@@ -22,14 +21,26 @@ class SignUpViewController: UIViewController {
 
         IQKeyboardManager.shared.enable = true
         
-        signUpButton.layer.masksToBounds = true
-        signUpButton.layer.cornerRadius = 3
+        signUpMailTextField.delegate = self
+        signUpPassTextField.delegate = self
+        signUpUserNameTextField.delegate = self
         
-        userImage.layer.masksToBounds = true
-        userImage.layer.cornerRadius = 75
-        userImage.layer.borderColor = UIColor.lightGray.cgColor
-        userImage.layer.borderWidth  = 0.1
+        signUpButton.layer.cornerRadius = 3
+        signUpButton.isEnabled = false
+        
+        profileImageButton.layer.masksToBounds = true
+        profileImageButton.layer.cornerRadius = 75
+        profileImageButton.layer.borderColor = UIColor.lightGray.cgColor
+        profileImageButton.layer.borderWidth  = 0.1
 
+    }
+    
+    // プロフィール画像の選択（フォトライブラリーへ遷移）
+    @IBAction func profileImageButtonAction(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        self.present(imagePickerController, animated: true, completion: nil)
     }
     
     // 新規登録処理
@@ -44,7 +55,36 @@ class SignUpViewController: UIViewController {
     
 }
 
+extension SignUpViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    // 写真が選択された時に呼ばれるメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[.editedImage] as? UIImage {
+            profileImageButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            profileImageButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
 extension SignUpViewController: UITextFieldDelegate {
+    // textFieldでテキスト選択が変更された時に呼ばれるメソッド
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        // textFieldが空かどうかの判別するための変数(Bool型)で定義
+        let mailIsEmpty = signUpMailTextField.text?.isEmpty ?? true
+        let passIsEmpty = signUpPassTextField.text?.isEmpty ?? true
+        let userNameIsEmpty = signUpUserNameTextField.text?.isEmpty ?? true
+        // 全てのtextFieldが記入済みの場合の処理
+        if mailIsEmpty || passIsEmpty || userNameIsEmpty {
+            signUpButton.isEnabled = false
+            signUpButton.backgroundColor = UIColor.systemGray2
+        } else {
+            signUpButton.isEnabled = true
+            signUpButton.backgroundColor = UIColor(red: 4/255, green: 185/255, blue: 2/255, alpha: 1)
+        }
+    }
+    
     // textField以外の部分を押したときキーボードが閉じる
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
